ed35e144f6d4bdd3cd4e0ba8bc516210dac74192
Sudhir Ray <66984542+Sudhir0228@users.noreply.github.com>
2024-05-10 21:16:14 -0500

    Update Routes.tsx

--- Assignments/MobileAPP/src/Routes.tsx
+++ Assignments/MobileAPP/src/Routes.tsx
@@ -1,1 +1,20 @@
 
+import { NavigationContainer } from "@react-navigation/native";
+import React from "react";
+
+import AppNavigator from "src/scenes/app/navigation/AppNavigator";
+
+import { getIsUserSigned } from "src/store/slice/applicationSlice";
+import { useAppSelector } from "src/store/storeHook";
+import AuthNavigator from "./scenes/auth/navigation/AuthNavigator";
+
+const Routes = () => {
+  const isUserSignedIn = useAppSelector(getIsUserSigned);
+  return (
+    <NavigationContainer>
+      {isUserSignedIn ? <AppNavigator /> : <AuthNavigator />}
+    </NavigationContainer>
+  );
+};
+
+export default Routes;
