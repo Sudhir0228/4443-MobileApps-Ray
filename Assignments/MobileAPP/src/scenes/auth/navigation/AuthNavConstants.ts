611140e6e73ceec5699d895487cf040e36095f61
Sudhir Ray <66984542+Sudhir0228@users.noreply.github.com>
2024-05-11 12:53:08 -0500

    Add files via upload

---
+++ Assignments/MobileAPP/src/scenes/auth/navigation/AuthNavConstants.ts
@@ -0,0 +1,11 @@
+export enum AuthNavScreenList {
+  LANDING_SCREEN = "LandingScreen",
+  LOGIN_SCREEN = "LoginScreen",
+  REGISTER_SCREEN = "RegisterScreen",
+}
+
+export type AuthStackParamList = {
+  LandingScreen: undefined;
+  LoginScreen: undefined;
+  RegisterScreen: undefined;
+};
